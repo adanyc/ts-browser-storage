917f072a8f30a30dca5c377559729ed40e2cb163
adanyc <adanyc@gmail.com>
2021-11-11 18:03:33 -0500

    test: add unit tests

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,4 @@ export { ContextStorage } from './context-storage';
 export { Strategy } from './strategy/strategy';
 export { LocalStorage } from './strategy/local-storage';
 export { SessionStorage } from './strategy/session-storage';
-export { MemoryStorage } from './strategy/memorys-torage';
+export { MemoryStorage } from './strategy/memory-storage';
--- src/test/localStorage.spec.ts
+++ src/test/localStorage.spec.ts
@@ -1,4 +1,4 @@
-import { LocalStorage} from '../strategy/localStorage';
+import { LocalStorage } from '../strategy/local-storage';
 
 describe('Unit test', () => {
   let localStorage: LocalStorage;
@@ -7,12 +7,27 @@ describe('Unit test', () => {
     localStorage = new LocalStorage();
   });
 
-  it('#should get the value', () => {
+  it('#should get', () => {
     localStorage.setItem('name', 'alex');
     expect(localStorage.getItem('name')).toBe('alex');
   });
 
-  it('#should throw an error when the key is empty', () => {
-    localStorage.setItem('', 'alex');
+  it('#should remove', () => {
+    localStorage.setItem('name', 'alex');
+    localStorage.removeItem('name');
+    const value = localStorage.getItem('name');
+    expect(value).toBeNull();
+  });
+
+  it('#should clear', () => {
+    localStorage.setItem('name', 'alex');
+    localStorage.setItem('name', 'daniel');
+    localStorage.clear();
+    expect(localStorage).toEqual({});
+  });
+
+  it('#should throw an error', () => {
+    const fn = () => localStorage.setItem(' ', 'alex');
+    expect(fn).toThrow(new Error('key should not be empty'));
   });
 });
